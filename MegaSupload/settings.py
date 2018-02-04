832ae16bbdca4231d81c886a2bb82bc527c0c710
djjoman21 <djjomail2@gmail.com>
2018-02-04 17:28:41 +0100

    add myDB.conf + requirements.txt

--- MegaSupload/settings.py
+++ MegaSupload/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'MegaSupload.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'read_default_file': 'myDB.conf'
+        },
     }
 }
 
---
+++ myDB.conf.tpl
@@ -0,0 +1,5 @@
+NAME = mydatabase
+USER = mydatabaseuser
+PASSWORD = mypassword
+HOST = 127.0.0.1
+PORT = 5432
