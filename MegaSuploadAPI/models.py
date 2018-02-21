1228ed8d4e7374bd460c5bf4f9198b4acd79cd60
nicolas <204343@supinfo.com>
2018-02-21 22:07:10 +0100

    add permission models

--- MegaSuploadAPI/models.py
+++ MegaSuploadAPI/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Permission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    read = models.BooleanField(default=True)
+    edit = models.BooleanField(default=True)
+    share = models.BooleanField(default=True)
+    owner = models.BooleanField(default=True)
