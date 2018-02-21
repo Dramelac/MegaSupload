200d469c27a7e5f8724b5c7bb37d41edfa9cb02a
Unknown <208108@supinfo.com>
2018-02-21 22:09:08 +0100

    Add Directory model

--- MegaSuploadAPI/models.py
+++ MegaSuploadAPI/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Directory (models.Model):
+    path = models.CharField(max_length=4096)
+    name =models.CharField(max_length=255)
+    parent  = models.ForeignKey('self',on_delete= models.CASCADE())
+    permission = models.ForeignKey(Permission,on_delete= models.CASCADE())
+
+    def test(self):
+        return self.name
+
+
+
