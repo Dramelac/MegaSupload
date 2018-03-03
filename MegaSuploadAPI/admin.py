57979f014638d3fbb4aa8e4daff0102e9d69f9fd
djjoman21 <djjomail2@gmail.com>
2018-03-03 16:49:53 +0100

    Extend User model + create needed models + create first migration

--- MegaSuploadAPI/admin.py
+++ MegaSuploadAPI/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Permission)
+admin.site.register(Directory)
+admin.site.register(File)
+admin.site.register(FileKey)
--- MegaSuploadAPI/models.py
+++ MegaSuploadAPI/models.py
@@ -1,29 +1,49 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    space_allowed = models.IntegerField(default=30)
+    pub_key = models.CharField(max_length=4096)
+    encrypted_priv_key = models.CharField(max_length=4096)
 
 
 class Permission(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     read = models.BooleanField(default=True)
-    edit = models.BooleanField(default=True)
-    share = models.BooleanField(default=True)
-    owner = models.BooleanField(default=True)
-    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    edit = models.BooleanField(default=False)
+    share = models.BooleanField(default=False)
+    owner = models.BooleanField(default=False)
 
-
-class File(models.Model):
-    path = models.CharField()
-    directory = models.ForeignKey(Directory, on_delete= models.CASCADE)
-    name = models.CharField(max_length=150)
-    type = models.CharField(max_length=150)
+    def __str__(self):
+        return "%s - Read: %d, Edit: %d, Share: %d, Owner: %d" % (self.user.username, self.read, self.edit, self.share, self.owner)
 
 
 class Directory (models.Model):
+    path = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    permissions = models.ManyToManyField(Permission)
+
+    def __str__(self):
+        return "%s - %s" % (self.path, self.name)
+
+
+class File(models.Model):
     path = models.CharField(max_length=4096)
-    name =models.CharField(max_length=255)
-    parent  = models.ForeignKey('self',on_delete= models.CASCADE)
-    permission = models.ForeignKey(Permission,on_delete= models.CASCADE)
+    directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    permissions = models.ManyToManyField(Permission)
+
+    def __str__(self):
+        return "%s - %s" % (self.path, self.name)
 
 
+class FileKey(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    key = models.CharField(max_length=4096)
+
+    def __str__(self):
+        return "%s - %s" % (self.owner.username, self.file.name)
