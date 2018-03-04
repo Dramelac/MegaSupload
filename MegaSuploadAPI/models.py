03b18d6397a3fd478af6c7413fb5d4946e22772e
djjoman21 <djjomail2@gmail.com>
2018-03-04 15:51:52 +0100

    refacto permission model

--- MegaSuploadAPI/models.py
+++ MegaSuploadAPI/models.py
@@ -8,22 +8,10 @@ class User(AbstractUser):
     encrypted_priv_key = models.TextField()
 
 
-class Permission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    read = models.BooleanField(default=True)
-    edit = models.BooleanField(default=False)
-    share = models.BooleanField(default=False)
-    owner = models.BooleanField(default=False)
-
-    def __str__(self):
-        return "%s - Read: %d, Edit: %d, Share: %d, Owner: %d" % (self.user.username, self.read, self.edit, self.share, self.owner)
-
-
 class Directory (models.Model):
     path = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
-    permissions = models.ManyToManyField(Permission)
 
     def __str__(self):
         return "%s - %s" % (self.path, self.name)
@@ -34,10 +22,22 @@ class File(models.Model):
     directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
-    permissions = models.ManyToManyField(Permission)
 
     def __str__(self):
         return "%s - %s" % (self.path, self.name)
+
+
+class Permission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    read = models.BooleanField(default=True)
+    edit = models.BooleanField(default=False)
+    share = models.BooleanField(default=False)
+    owner = models.BooleanField(default=False)
+    file = models.ForeignKey(File, on_delete=models.CASCADE, blank=True, null=True)
+    directory = models.ForeignKey(Directory, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return "%s - Read: %d, Edit: %d, Share: %d, Owner: %d" % (self.user.username, self.read, self.edit, self.share, self.owner)
 
 
 class FileKey(models.Model):
