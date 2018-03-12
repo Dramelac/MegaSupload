ae502d324e20491e85cbb9dc5a6f2e384a193d58
Unknown <204040@supinfo.com>
2018-03-12 21:56:08 +0100

    FIleKeyDAO add/remove/get OK

--- MegaSuploadAPI/DAL/FileKeyDAO.py
+++ MegaSuploadAPI/DAL/FileKeyDAO.py
@@ -1,3 +1,24 @@
+from django.core.exceptions import ObjectDoesNotExist
 
-# TODO Add FileKey
-# TODO Remove FileKey
+from MegaSuploadAPI.models import FileKey
+import random
+import string
+
+
+def newFileKey(owner, file):
+    key = ''.join(random.choice(string.ascii_letters + string.digits))
+    FileKey.objects.create(owner=owner, file=file, key=key)
+
+
+def remove(owner, file):
+    key = FileKey.objects.get(file=file, owner=owner)
+    if key is not None:
+        key.delete()
+
+
+def getFileKey(owner, file):
+    fileKey = FileKey.objects.get(file=file, owner=owner)
+    if fileKey is not None:
+        return fileKey
+    else:
+        raise ObjectDoesNotExist
