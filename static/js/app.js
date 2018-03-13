b944f484450ffa2b4a7a6aeba73c4938949a627d
djjoman21 <djjomail2@gmail.com>
2018-03-13 16:59:13 +0100

    add directory navigation

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,12 @@
-var test = new Vue({
+async function loadDir(dirId) {
+    this.loader = true;
+    var data = await $.getJSON('/api/file/list_item?did=' + (dirId || ''));
+    this.files = data.file;
+    this.directories = data.directory;
+    this.loader = false;
+    $("#path").text("/")
+}
+var fileManager = new Vue({
     el: '#fileApp',
     delimiters: ['[[', ']]'],
     data: {
@@ -6,15 +14,11 @@ var test = new Vue({
         files: [],
         loader: true
     },
-    mounted: async function () {
-        var data = await $.getJSON('/api/file/list_item');
-        this.files = data.file;
-        this.directories = data.directory;
-        this.loader = false
-    },
+    mounted: loadDir,
     methods: {
         fileClicked: function (fileId, dirId) {
             window.open('/api/file/download?did=' + dirId + '&fid=' + fileId,'_blank');
-        }
+        },
+        openDir: loadDir
     }
 });
