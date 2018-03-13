6d55e7f0890d6a9392ce3189067568c2b944e46d
djjoman21 <djjomail2@gmail.com>
2018-03-13 17:44:54 +0100

    add mapping between mime type and icons

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,29 @@
+function getFileIcon(mimeType) {
+    var iconClasses = {
+        'image': 'fa-file-image',
+        'audio/mpeg': 'fa-file-audio',
+        'video': 'fa-file-video',
+        'application/pdf': 'fa-file-pdf',
+        'application/msword': 'fa-file-word',
+        'application/vnd.ms-word': 'fa-file-word',
+        'application/vnd.oasis.opendocument.text': 'fa-file-word',
+        'application/vnd.openxmlformatsfficedocument.wordprocessingml': 'fa-file-word',
+        'application/vnd.ms-excel': 'fa-file-excel',
+        'application/vnd.openxmlformatsfficedocument.spreadsheetml': 'fa-file-excel',
+        'application/vnd.oasis.opendocument.spreadsheet': 'fa-file-excel',
+        'application/vnd.ms-powerpoint': 'fa-file-powerpoint',
+        'application/vnd.openxmlformatsfficedocument.presentationml': 'fa-file-powerpoint',
+        'application/vnd.oasis.opendocument.presentation': 'fa-file-powerpoint',
+        'text/plain': 'fa-file-text',
+        'text/html': 'fa-file-code',
+        'application/json': 'fa-file-code',
+        'application/gzip': 'fa-file-archive',
+        'application/zip': 'fa-file-archive',
+    };
+
+    return iconClasses[mimeType] || 'fa-file';
+}
+
 async function loadDir(dirId, dirName) {
     this.loader = true;
     var data = await $.getJSON('/api/file/list_item?did=' + (dirId || ''));
@@ -29,6 +55,7 @@ var fileManager = new Vue({
         fileClicked: function (fileId, dirId) {
             window.open('/api/file/download?did=' + dirId + '&fid=' + fileId,'_blank');
         },
-        openDir: loadDir
+        openDir: loadDir,
+        getFileIcon: getFileIcon
     }
 });
